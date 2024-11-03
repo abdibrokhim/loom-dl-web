0b117a67cd5845ca24f4938852fbbfa2f90fde94
abdibrokhim <abdibrokhim@gmail.com>
2024-11-04 01:54:38 +0500

    added topmate profile

--- src/app/components/LoomDownloader.tsx
+++ src/app/components/LoomDownloader.tsx
@@ -99,6 +99,19 @@ const LoomDownloader = () => {
             <div className='relative bottom-[-200px]'> 
                 <Footer />
             </div>
+            <script
+                src="https://topmate-embed.s3.ap-south-1.amazonaws.com/v1/topmate-embed.js"
+                user-profile="https://topmate.io/embed/profile/abdibrokhim?theme=D5534D"
+                btn-style='{"backgroundColor":"#fff","color":"#000","border":"1px solid #000"}'
+                embed-version="v1"
+                button-text="Hey, dear! Let's chat."
+                position-right="30px"
+                position-bottom="30px"
+                custom-padding="0px"
+                custom-font-size="16px"
+                custom-font-weight="500"
+                custom-width="200px"
+            ></script>
         </div>
     );
 };
