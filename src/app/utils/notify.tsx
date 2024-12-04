f9c0447ff211ce6b7c223a109b0d2cb392963f68
abdibrokhim <abdibrokhim@gmail.com>
2024-12-04 19:00:01 +0500

    notify fixed

--- src/app/utils/notify.tsx
+++ src/app/utils/notify.tsx
@@ -17,7 +17,7 @@ const Notification: React.FC<NotificationProps> = ({ message, type, onClose }) =
   const bgColor = type === 'error' ? 'bg-[#f84f31]' : type === 'success' ? 'bg-[#23c552]' : 'bg-[#1e90ff]';
 
   return (
-    <div className={`fixed text-xs sm:text-md top-10 left-1/2 transform -translate-x-1/2 ${bgColor} text-white px-4 py-2 rounded-md shadow-lg z-50`}>
+    <div className={`fixed w-[300px] text-xs sm:text-md top-10 left-1/2 transform -translate-x-1/2 ${bgColor} text-white px-4 py-2 rounded-md shadow-lg z-50`}>
       <p>{message}</p>
     </div>
   );
